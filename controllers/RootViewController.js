653197d126832b556c8a0862131fcfa7815ad17f
eejdan <65033345+eejdan@users.noreply.github.com>
2022-05-18 09:23:47 +0300

    a

--- controllers/RootViewController.js
+++ controllers/RootViewController.js
@@ -6,11 +6,11 @@ class RootViewController {
         setInterval(updatePopularUniversitiesList,  (10*60*1000));
     }
     static async updatePopularUniversitiesList() {
-        
+        return;
     }
     static async getLandingPage(req, res, next) {
         res.render('index');
     }
 }
 
-module.exports = RootViewController
+module.exports = RootViewController;
